d29d2232d61a7476660a88a2a8e1626670b4c64d
manav <toremanav@gmail.com>
2023-07-13 07:33:42 +0530

    Updated History.dart

--- lib/pages/History.dart
+++ lib/pages/History.dart
@@ -16,6 +16,13 @@ class _CalenderPageState extends State<CalenderPage> {
         body: Column(
           children: [
             TableCalendar(
+              headerStyle: const HeaderStyle(
+                formatButtonVisible: false,
+                titleCentered: true,
+              ),
+              availableCalendarFormats: const {
+                CalendarFormat.month: 'week',
+              },
               locale: "en_US",
               firstDay: DateTime.utc(2010, 10, 16),
               lastDay: DateTime.utc(2030, 3, 14),
